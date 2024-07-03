257310095486aa209086b0a0882b6602ffab00c6
Ingrith Santamaria <ingrithsantamaria@Ingrith-Santamaria-MacBook-Air.local>
2024-07-03 08:44:54 -0500

    Update Cypress Version

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://www.adidas.co",
+    // baseUrl: "https://www.adidas.co/",
     chromeWebSecurity: false,
     failOnStatusCode: false,
     setupNodeEvents(on, config) {
